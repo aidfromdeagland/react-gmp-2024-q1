4757a0947142ee9a70c6d6c36a8a612473c90d52
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-03-05 09:14:22 +0200

    feat: add Counter component story

---
+++ src/stories/Counter.stories.jsx
@@ -0,0 +1,40 @@
+import { within, userEvent, expect } from '@storybook/test';
+import Counter from "../counter/Counter";
+
+export default {
+    title: 'Counter',
+    component: Counter,
+    parameters: {
+      layout: 'centered',
+    },
+};
+
+export const initialCounterProp = {
+  args: {
+    initialCounter: 42
+  },
+};
+
+export const IncrementInteraction = {
+    play: async ({ canvasElement }) => {
+      const canvas = within(canvasElement);
+      const incrementButton = canvas.getByText('+');
+      const textNode = canvas.getByText((content, element) => element.tagName.toLowerCase() === 'span');
+      const currentValue = Number(textNode.textContent);
+      await expect(incrementButton).toBeInTheDocument();
+      await userEvent.click(incrementButton);
+      await expect(canvas.getByText(`${currentValue + 1}`)).toBeInTheDocument();
+    },
+};
+
+export const DecrementInteraction = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const decrementButton = canvas.getByText('-');
+    const textNode = canvas.getByText((content, element) => element.tagName.toLowerCase() === 'span');
+    const currentValue = Number(textNode.textContent);
+    await expect(decrementButton).toBeInTheDocument();
+    await userEvent.click(decrementButton);
+    await expect(canvas.getByText(`${currentValue - 1}`)).toBeInTheDocument();
+  },
+};
