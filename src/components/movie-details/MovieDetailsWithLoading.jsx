aaeb6a04962bce526da5aa17c7634b54e3a1db91
Stanislau Laniuk <aidfromdeagland@gmail.com>
2024-03-24 20:02:45 +0200

    feat: add back-to-search button

--- src/components/movie-details/MovieDetailsWithLoading.jsx
+++ src/components/movie-details/MovieDetailsWithLoading.jsx
@@ -1,15 +1,23 @@
 import { Suspense } from "react";
-import { useLoaderData, Await } from "react-router-dom";
+import { useLoaderData, Await, useNavigate, useLocation } from "react-router-dom";
 import MovieDetails from "./MovieDetails";
 
 const MovieDetailsWithLoading = () => {
   const data = useLoaderData();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  return <Suspense fallback={<p>Loading...</p>}>
-    <Await resolve={data.movie} errorElement={<p>Oops</p>}>
-      {(movie) => <MovieDetails movie={movie} />}
-    </Await>
-  </Suspense>
+  return <>
+    <button onClick={() => {
+      const navigationRoute = location.search ? `../${location.search}` : '..';
+      navigate(navigationRoute, { relative: 'path' });
+    }}>Back To Search</button>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Await resolve={data.movie} errorElement={<p>Oops</p>}>
+        {(movie) => <MovieDetails movie={movie} />}
+      </Await>
+    </Suspense>
+  </>
 }
 
 export default MovieDetailsWithLoading;
